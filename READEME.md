c7b2fbfaf9082a3b68cb037da204a3b04e593b61
Erica Cheong <erica.clw@gmail.com>
2018-05-11 04:25:54 +0800

    Initial commit

---
+++ READEME.md
@@ -0,0 +1,1 @@
+# Solutions for Haskell Programming from First Principles
---
+++ WordNumber.hs
@@ -0,0 +1,29 @@
+-- Solution for Haskell Programming from first principles
+-- Chapter 8 Numbers into Words
+
+module WordNumber where
+
+import Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord n =
+    case n of
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        5 -> "five"
+        6 -> "six"
+        7 -> "seven"
+        8 -> "eight"
+        9 -> "nine"
+        otherwise -> "not a number"
+
+
+digits :: Int -> [Int]
+digits n 
+    | n < 10 = [n `mod` 10]
+    | otherwise = digits (div n 10) ++ digits (mod n 10)
+
+wordNumber :: Int -> String
+wordNumber n = reverse $ tail $ reverse $ concat $ map (++ "-") $ map digitToWord $ digits n
